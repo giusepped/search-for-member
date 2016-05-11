7ad011a76af8b12200bc9501fa11f2f40539eb69
giusepped <giusdesan@gmail.com>
2016-05-11 18:12:02 +0100

    added first feature test

---
+++ spec/features/member_feature_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+feature 'members' do
+
+	context 'no search has been made yet' do
+		scenario 'should display an input box and a "Find" button' do
+			visit members_path
+			expect(page).to have_selector('input[name="search"]')
+			expect(page).to have_button 'Find'
+		end
+	end
+
+end
